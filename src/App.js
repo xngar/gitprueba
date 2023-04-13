1b0273064ffacad581e32c178841c0bbd88a932d
Mauricio Zuñiga <mantonio.zr@gmail.com>
2023-04-13 16:35:58 -0400

    se quitaron cosas del home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import {v4 as uuid} from "uuid"
 
@@ -6,9 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          
           Aqui tirando un número random: {uuid()}
         </p>
         <h4>Hubo un cambio      </h4>
